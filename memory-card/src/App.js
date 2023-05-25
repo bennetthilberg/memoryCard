14112ffdfd4d64fd1f6fa9f5b0b480c57ccda66f
bennetthilberg <bennett.hilberg@gmail.com>
2023-05-24 20:32:10 -0500

    Add useEffects for score and letter array, create card click handling function that is passed to Card components

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -4,16 +4,45 @@ import CurScore from './components/CurScore';
 import BestScore from './components/BestScore';
 import ScoreTester from './components/ScoreTester';
 import Card from './components/Card';
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 
 function App() {
   const [pastScores, setPastScores] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [usedLetters, setUsedLetters] = useState([]);
+  const [score, setScore] = useState(0);
+  function getRandomCapitalLetter() {
+    const charCode = Math.floor(Math.random() * 26) + 65;
+    return String.fromCharCode(charCode);
+  }
+  useEffect(() => {
+    let initialCards = [];
+    for(let i = 0; i < 10; i++) {
+      initialCards.push(getRandomCapitalLetter());
+    }
+    setCards(initialCards);
+  }, [])
+  useEffect(() => {
+    setPastScores([...pastScores, score]);
+  }, [score])
+  function useLetter(l){
+    if(usedLetters.includes(l)){
+      setScore(0);
+    }
+    else{
+      setUsedLetters(prevUsedLetters => [...prevUsedLetters, l]);
+      setScore(prevScore => prevScore + 1);
+    }
+  }
   return (
     <div className="App">
-      <CurScore score=''/>
+      <CurScore score={score}/>
       <BestScore pastScores={pastScores} />
       {/*<ScoreTester oldScores = {pastScores} setScore={setPastScores}/>*/}
-      <Card letter={'A'}/>
+      {/*<Card letter={'A'}/>*/}
+      <div className="container">
+        {cards.map((card, i) => <Card key={i} letter={card} handlePick={useLetter}/>)}
+      </div>
     </div>
   );
 }
--- memory-card/src/components/Card.js
+++ memory-card/src/components/Card.js
@@ -1,9 +1,9 @@
 import { getDefaultNormalizer } from '@testing-library/react';
 import React, {useEffect,useState} from 'react';
 
-export default function Card({letter}){
+export default function Card({letter, handlePick}){
     return(
-        <div className="card">
+        <div className="card" onClick={() => handlePick(letter)}>
             <p>{letter}</p>
         </div>
     )
